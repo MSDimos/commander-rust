7b7740e112b54e47649c1a54dc54ec01ea754d01
MSDimos <DoubleDimos@outlook.com>
2019-04-29 22:34:07 +0800

    chore: better example
    fill a better example

--- examples/cli.rs
+++ examples/cli.rs
@@ -4,15 +4,42 @@ use commander_rust::{ option, command, Cli, entry, run };
 
 #[option(-c, --cn, "Chinese")]
 #[option(-e, --en, "English")]
-#[command(say <word>, "Say hello")]
-fn say(word: String, cli: Cli) {
+#[option(-j, --jp, "English")]
+#[option(-r, --ru, "Russian")]
+#[option(-f, --fr, "French")]
+#[option(-n, --name <name>, "Who I am?")]
+#[command(hello, "Say hello")]
+fn hello(cli: Cli) {
     if cli.has("cn") {
-        println!("词语是{}", word);
+        println!("你好，世界");
     } else if cli.has("en") {
-        println!("word is {}", word);
+        println!("hello, world!");
+    } else if cli.has("jp") {
+        println!("こんにちは、世界");
+    } else if cli.has("ru") {
+        println!("Здравствуй, мир");
+    } else if cli.has("fr") {
+        println!("Salut, le monde.");
+    }
+
+    if cli.has("peace") || !cli.has("peace") {
+        println!("Whether the world is peaceful or not, I still love peace.")
+    }
+
+    let who =  cli.get_or("name", String::from("Double Dimos"));
+
+    println!("I am {} ❤", who);
+}
+
+
+#[command(wish <who>, "best wishes")]
+fn wish(who: String, cli: Cli) {
+    if !cli.has("peace") {
+        println!("Best wishes to you, from {}", who);
     }
 }
 
+#[option(-p, --peace, "I love peace")]
 #[entry]
 fn main() {
     run!();
